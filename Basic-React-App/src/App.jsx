90acbcd142a966d7e60a5b59eba3ebe01a4c3e9b
Satyam Raghuvanshi <satyamraghuvanshi220ct@gmail.com>
2026-05-09 14:04:37 +0530

    chore: update node_modules dependencies by installing missing packages

---
+++ Basic-React-App/src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import Title from "./assets/Title.jsx";
+
+
+
+
+function App() {
+  return (
+    <div>
+      <h1>This is my App Component</h1>
+      <p>inside app component we have :</p>
+      <Title/>
+    </div>
+  );
+}
+
+export default App
